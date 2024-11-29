0003fd4417e418a6d7f4790ba3a702b49069c4b8
liyao-l-y <2034433477@qq.com>
2024-11-29 17:33:43 +0800

    2024.11.29
    native指纹

--- app/src/main/cpp/app1.cpp
+++ app/src/main/cpp/app1.cpp
@@ -1,29 +1,75 @@
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("app1");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("app1")
-//      }
-//    }
-
 #include <cstdio>
 #include <cstdlib>
 #include <jni.h>
+#include <sys/system_properties.h>
+#include <media/NdkMediaDrm.h>
+#include <cstring>
+
 #include "com_example_app1_fingerprintjni.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_app1_fingerprintjni_fingerprint(JNIEnv *env, jobject){
-    char *text = "I am from c";
-    return (*env).NewStringUTF(text);
+
+    const char *properties[] = {
+            "ro.build.fingerprint",
+            "ro.build.build.fingerprint",
+            "ro.bootimage.build.fingerprint",
+            "ro.odm.build.fingerprint",
+            "ro.product.build.fingerprint",
+            "ro.system_ext.build.fingerprint",
+            "ro.system.build.fingerprint",
+            "ro.vendor.build.fingerprint",
+            "ro.build.description",
+
+            "ro.product.board",
+            "ro.bootloader",
+            "ro.product.brand",
+            "ro.product.cpu.abi",
+            "ro.product.device",
+            "ro.build.display.id",
+            "ro.hardware",
+            "ro.build.host",
+            "ro.build.id",
+            "ro.product.model",
+            "ro.product.manufacturer",
+            "ro.product.name",
+            "ro.product.radio",
+            "ro.build.tags",
+            "ro.build.time",
+            "ro.build.type",
+            "ro.build.user",
+            "ro.build.version.release",
+            "ro.build.version.codename",
+            "ro.build.version.incremental",
+            "ro.build.version.sdk",
+            "ro.build.version.sdk_int"
+    };
+
+    int numProperties = sizeof(properties) / sizeof(properties[0]);
+    char *result = (char *)malloc(4096); // 分配足够的内存以存储结果
+    if (result == nullptr) {
+        return nullptr; // 处理内存分配失败的情况
+    }
+    result[0] = '\0'; // 初始化字符串
+
+    for (int i = 0; i < numProperties; i++) {
+        char value[PROP_VALUE_MAX] = {0};
+        __system_property_get(properties[i], value);
+
+        if (strlen(value) == 0) {
+            snprintf(value, sizeof(value), "无结果");
+        }
+
+        // 将属性名和属性值合并到结果字符串中
+        snprintf(result + strlen(result), 4096 - strlen(result), "\n%s: %s", properties[i], value);
+    }
+
+    // 创建 Java 字符串并返回
+    jstring jResult = (*env).NewStringUTF(result);
+
+    // 清理
+    free(result); // 释放分配的内存
+
+    return jResult; // 返回合并后的字符串
 }
